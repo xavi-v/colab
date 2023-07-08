47a543fea89d0310665de5d94bd31607242c9b2d
xavi-v v <javivergara@udec.cl>
2023-07-08 15:35:48 +0000

    sabado 11:35

--- colabapp/colabapp/settings.py
+++ colabapp/colabapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#ogz6_dfwv&k_9)^m$m77_5uvl^i5ekzt9=%9%!n1z-1452w9*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'academy.apps.AcademyConfig',
+    'security.apps.SecurityConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'colabapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./colabapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- colabapp/colabapp/urls.py
+++ colabapp/colabapp/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from academy.views import v_index
 
 urlpatterns = [
+    path('', v_index),
     path('admin/', admin.site.urls),
+
+
+    path('academy/', include ('academy.urls')), #modulo
+    path('security/', include ('security.urls')), #modulo
 ]
