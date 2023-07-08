2b4913e9a4ed289d8f4adad4b950c3b90aca6178
xavi-v v <javivergara@udec.cl>
2023-07-08 16:46:21 +0000

    buuu

--- colabapp/colabapp/settings.py
+++ colabapp/colabapp/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os 
+from pathlib import Path 
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIR = [os.path.join(BASE_DIR, "colabapp/static")]
+print(STATICFILES_DIR)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
